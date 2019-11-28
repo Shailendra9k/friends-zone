f7fb3379f4d6d1bf84f3d255539b91ef0c4fc07d
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-11-28 11:34:18 +0200

    setup personList component

--- src/App.js
+++ src/App.js
@@ -24,20 +24,28 @@ const PersonList = () => {
 
   return (
     <section>
-      <Person person={people[0]} />;
+      <Person person={people[0]}>
+        A Frontend developer working on various technologies mainly ReactJs as
+        major framework. Along with it working on backend technologies like
+        NodeJs, MongoDB and Express.js.
+      </Person>
+      ;
       <Person person={people[1]} />;
       <Person person={people[2]} />;
     </section>
   );
 };
-const Person = () => {
-  const url = "https://randomuser.me/api/portraits/thumb/men/22.jpg";
+const Person = props => {
+  const { img, name, job } = props.person;
+  const { children } = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
   return (
     <div className="person">
       <img src={url} alt="person" />
       <div>
-        <h4>name</h4>
-        <h4>job</h4>
+        <h4>{name}</h4>
+        <h4>{job}</h4>
+        {children}
       </div>
     </div>
   );
