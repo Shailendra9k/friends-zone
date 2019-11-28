6c3ab5ba7954569c2b944311fe19729b0ed86ccf
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-11-28 11:21:16 +0200

    creating person component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => <PersonList />;
+
+const PersonList = () => {
+  const people = [
+    {
+      img: 22,
+      name: "Shailendra",
+      job: "developer"
+    },
+    {
+      img: 34,
+      name: "Dhiraj",
+      job: "product manager"
+    },
+    {
+      img: 56,
+      name: "Dilip",
+      job: "designer"
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section>
+      <Person person={people[0]} />;
+      <Person person={people[1]} />;
+      <Person person={people[2]} />;
+    </section>
+  );
+};
+const Person = () => {
+  const url = "https://randomuser.me/api/portraits/thumb/men/22.jpg";
+  return (
+    <div className="person">
+      <img src={url} alt="person" />
+      <div>
+        <h4>name</h4>
+        <h4>job</h4>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
